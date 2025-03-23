5a81cb8bd6ebd2179d4aa4065a3987a642046b3a
Isbha Khalid <isbhakhalid19@gmail.com>
2025-03-23 13:58:00 +0400

    changed the names to Db migration

--- Db Migration/DbMigration.Designer.cs
+++ Db Migration/DbMigration.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Db_Migration
 {
-    partial class Form1
+    partial class DbMigration
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // DbMigration
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "DbMigration";
+            Text = "Db Migration";
+            ResumeLayout(false);
         }
 
         #endregion
--- Db Migration/DbMigration.cs
+++ Db Migration/DbMigration.cs
@@ -1,8 +1,8 @@
 namespace Db_Migration
 {
-    public partial class Form1 : Form
+    public partial class DbMigration : Form
     {
-        public Form1()
+        public DbMigration()
         {
             InitializeComponent();
         }
